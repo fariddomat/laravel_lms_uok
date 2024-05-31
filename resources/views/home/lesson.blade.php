31568780cb6fb7571f278a4690f7765cd0c75e0a
Farid Domat <fariddomat.000@gmail.com>
2024-06-01 01:16:46 +0300

    files

--- resources/views/home/lesson.blade.php
+++ resources/views/home/lesson.blade.php
@@ -23,11 +23,7 @@
         <div class="row">
             <div class="col-md-8 offset-md-2 wow slideInDown" data-wow-delay="0.1s">
 
-                <!-- Lesson Image -->
-                {{-- <img src="img/course-1.jpg" alt="Lesson Image" class="img-fluid mb-4"> --}}
-
                 <!-- Lesson Title -->
-
                 <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
                     <h6 class="section-title bg-white text-center text-primary px-3">الدرس </h6>
                     <h1 class="mb-5">محتوى الدرس</h1>
@@ -41,14 +37,18 @@
                     {!! $lesson->content !!}
                 </div>
 
+                @if ($lesson->lesson_files->count() >0)
                 <!-- Files List -->
                 <div class="files-list mt-4 wow fadeInUp" data-wow-delay="0.2s">
                     <h3 class="mb-2">الملفات</h3>
-                    <ul>
-                        <li><a href="file1.pdf">الملف الأول</a></li>
-                        <li><a href="file2.docx">الملف الثاني</a></li>
+                    <ul class="list-group">
+                        @foreach ($lesson->lesson_files as $file)
+                            <li class="list-group-item"><a href="{{ asset('storage/' . $file->path) }}">{{ $file->title }}</a></li>
+                        @endforeach
                     </ul>
                 </div>
+
+                @endif
 
                 <!-- Favorite Button -->
                 <div class="favorite-section mt-4 wow fadeInUp" data-wow-delay="0.3s">
@@ -76,6 +76,7 @@
                         <p>Please <a href="{{ route('login') }}">سجل دخول</a> للإضافة إلى المفضلة.</p>
                     @endauth
                 </div>
+
                 <!-- Comments Section -->
                 <div class="comments-section mt-5 wow fadeInUp" data-wow-delay="0.3s">
                     <h3 class="mb-3">التعليقات</h3>
@@ -116,41 +117,8 @@
                         <p>Please <a href="{{ route('login') }}">سجل الدخول</a> لتتمكن من التعليق.</p>
                     @endauth
                 </div>
-                <!-- FAQ Section -->
-                <div class="faq-section mt-5 wow fadeInUp" data-wow-delay="0.2s">
-                    <h3 class="mb-3">الأسئلة الشائعة</h3>
-                    <div class="accordion" id="faqAccordion">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingOne">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                                    السؤال الأول
-                                </button>
-                            </h2>
-                            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
-                                data-bs-parent="#faqAccordion">
-                                <div class="accordion-body">
-                                    إجابة السؤال الأول.
-                                </div>
-                            </div>
-                        </div>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingOne">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseOne1" aria-expanded="false" aria-controls="collapseOne1">
-                                    السؤال الثاني
-                                </button>
-                            </h2>
-                            <div id="collapseOne1" class="accordion-collapse collapse" aria-labelledby="headingOne"
-                                data-bs-parent="#faqAccordion">
-                                <div class="accordion-body">
-                                    إجابة السؤال الثاني.
-                                </div>
-                            </div>
-                        </div>
-                        <!-- ... more FAQ items -->
-                    </div>
-                </div>
+
+
 
             </div>
         </div>
